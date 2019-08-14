11a6ca1a44923ac99e6c5b0c939125ffd710fc6f
Fabian Simon <fabian.simon@hornbach.com>
2019-08-14 15:34:22 +0200

    Simple example

--- dist/index.js
+++ dist/index.js
@@ -1,26 +1,24 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const funk_js_agent_1 = require("funk-js-agent");
-const con = new funk_js_agent_1.funkAgent("ws://127.0.0.1:3000", "changeMe04cf242924f6b5f96");
-con.connect((connection) => {
-    setInterval(() => {
-        const ok = connection.send([{
-                attr: {
-                    container: "test js con",
-                    container_id: "12344",
-                    hostname: "localhost",
-                    namespace: "",
-                    service: "",
-                },
-                data: [JSON.stringify({
-                        level: "warn",
-                        nana: "bubu",
-                        message: "Test"
-                    })],
-                searchindex: "test-js-agent",
-                time: new Date(Date.now()),
-                type: funk_js_agent_1.MessageType.Log,
-            }]);
-        console.log("send data", ok);
-    }, 3000);
-});
+const funk_agent_1 = require("funk-agent");
+const con = new funk_agent_1.FunkAgent("ws://127.0.0.1:3000", "changeMe04cf242924f6b5f96");
+setInterval(() => {
+    const ok = con.send([{
+            attr: {
+                container: "test js con",
+                container_id: "12344",
+                hostname: "localhost",
+                namespace: "",
+                service: "",
+            },
+            data: [JSON.stringify({
+                    level: "warn",
+                    nana: "bubu",
+                    message: "Test"
+                })],
+            searchindex: "test-js-agent",
+            time: new Date(Date.now()),
+            type: funk_agent_1.MessageType.Log,
+        }]);
+    console.log("send data", ok);
+}, 3000);
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,23 @@
-import {funkAgent, MessageType} from 'funk-js-agent'
+import {FunkAgent, MessageType} from 'funk-agent'
 
 
-const con = new funkAgent("ws://127.0.0.1:3000","changeMe04cf242924f6b5f96")
-
-con.connect((connection) => {
-  setInterval(() => {
-    const ok = connection.send([{
-      attr: {
-        container: "test js con",
-        container_id: "12344",
-        hostname: "localhost",
-        namespace:"",
-        service: "",
-      },
-      data:[JSON.stringify({
-        level: "warn",
-        nana: "bubu",
-        message: "Test"
-      })],
-      searchindex: "test-js-agent",
-      time: new Date(Date.now()),
-      type: MessageType.Log,
-    }])
-    console.log("send data", ok )
-  }, 3000)
-})
+const con = new FunkAgent("ws://127.0.0.1:3000","changeMe04cf242924f6b5f96")
+setInterval(() => {
+  con.send([{
+    attr: {
+      container: "test js con",
+      container_id: "12344",
+      hostname: "localhost",
+      namespace:"",
+      service: "",
+    },
+    data:[JSON.stringify({
+      level: "warn",
+      nana: "bubu",
+      message: "Test"
+    })],
+    searchindex: "test-js-agent",
+    time: new Date(Date.now()),
+    type: MessageType.Log,
+  }])
+}, 3000)
